2e3024ad0aca2543aa9edb7686c1a8083c7f3fd1
SBMOYO <sunganomoyo1@gmail.com>
2024-10-07 14:20:01 +0200

    fix allowed host settings

--- House_renting/settings.py
+++ House_renting/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sku$l#_yli5t)&ax8(@-@0205x+xjd=abn30l46mm9*lk3rr_4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
