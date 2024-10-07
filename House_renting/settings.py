451881952fa5fa2a32c7ad1f4eaab1518ac00303
SBMOYO <sunganomoyo1@gmail.com>
2024-10-07 14:32:37 +0200

    debugging

--- House_renting/settings.py
+++ House_renting/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sku$l#_yli5t)&ax8(@-@0205x+xjd=abn30l46mm9*lk3rr_4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
